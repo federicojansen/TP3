9e9294b094e72071458fd14be96f16fbc1b6371d
federicojansen <federicojansen@gmail.com>
2016-11-29 02:26:04 -0200

    ignorar commit

---
+++ app/src/main/java/com/example/federico/tp3/ListarRoupa.java
@@ -0,0 +1,51 @@
+package com.example.federico.tp3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by federico on 29/11/2016.
+ */
+
+public class ListarRoupa extends AppCompatActivity {
+
+    Button button_load;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listar_roupas);
+
+        /**
+         * clique botao carregar galeria
+         */
+        button_load.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/federico/tp3/MainActivity.java
+++ app/src/main/java/com/example/federico/tp3/MainActivity.java
@@ -1,13 +1,80 @@
 package com.example.federico.tp3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_evento, button_gerar, button_roupa, button_listar_roupas, button_listar_eventos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_evento = (Button) findViewById(R.id.button_evento);
+        button_gerar = (Button) findViewById(R.id.button_gerar);
+        button_roupa = (Button) findViewById(R.id.button_roupa);
+        button_listar_roupas = (Button) findViewById(R.id.button_listar_roupas);
+        button_listar_eventos = (Button) findViewById(R.id.button_listar_eventos);
+
+
+        /**
+         * clique botao gerar roupa
+         */
+        button_gerar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent it = new Intent(MainActivity.this, CriaRoupa.class);
+                startActivity(it);
+            }
+        });
+
+        /**
+         * clique botao adicionar roupa
+         */
+        button_roupa.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent it = new Intent(MainActivity.this, CriaRoupa.class);
+                startActivity(it);
+            }
+        });
+
+        /**
+         * clique botao adicionar evento
+         */
+        button_evento.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent it = new Intent(MainActivity.this, CriaRoupa.class);
+                startActivity(it);
+            }
+        });
+
+        /**
+         * clique botao listar roupas
+         */
+        button_listar_roupas.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent it = new Intent(MainActivity.this, CriaRoupa.class);
+                startActivity(it);
+            }
+        });
+
+        /**
+         * clique botao listar eventos
+         */
+        button_listar_eventos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent it = new Intent(MainActivity.this, CriaRoupa.class);
+                startActivity(it);
+            }
+        });
     }
 }
